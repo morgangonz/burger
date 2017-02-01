821071c788d84e42e4c8461d098200b02356bb14
Morgan Gonzalez <Morgan@Morgans-MacBook-Pro.local>
2017-01-31 21:46:31 -0500

    working

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,8 @@ USE shark_db;
 CREATE TABLE food
 (
 	id int NOT NULL AUTO_INCREMENT,
-	item varchar(255) NOT NULL,
+	item varchar(255) NOT NULL, 
+	devoured tinyint(1) DEFAULT 0,
+	`date` timestamp NOT NULL DEFAULT CURRENT_timestamp ON UPDATE CURRENT_timestamp,
 	PRIMARY KEY (id)
 );
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-//var methodOverride = require("method-override");
-
-
 var app = express();
 var port = 8675;
-
-
 
 // Serve static content for the app to the browser:: (process.cwd)vs(_dirname +) ???
 app.use(express.static(process.cwd() + "/"));
@@ -16,9 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 ///requiring handlebars 
 var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
 
 //mysql
 var mysql = require("mysql");
@@ -36,25 +30,30 @@ var connection = mysql.createConnection({
 
 ///route
 app.get("/", function(req, res) {
-
-    connection.query("SELECT * FROM food;", function(err, data) {
-      if (err) throw err;
-
-      res.render("index", { food : data });
-    });
+  connection.query("SELECT * FROM food;", function(err, data) {
+    if (err) throw err;
+    res.render("index", { food : data });
+  });
 });
 
-//????/////
+///////
 app.post("/", function(req, res) {
-
-connection.query("INSERT INTO food (item) VALUES (?)", [req.body.event], function(err, result) {
+  connection.query("INSERT INTO food (item) VALUES (?)", [req.body.event], function(err, result) {
     if (err) throw err;
-
     res.redirect("/");
   });
 
 });
-//////
+
+app.post("/devoured/:id", function(req, res) {
+  var id = req.params.id
+
+  connection.query("UPDATE food SET devoured = 1 WHERE id = ?", id, function(err, results){
+    if (err) throw err;
+    res.redirect("/");
+  })  
+})
+///////
 app.listen(port, function() {
   console.log("Listening on port " + port)
 });
